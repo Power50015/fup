f5d3788233183de8c7473e3f5beba87c3206c40f
Power50015 <powermostafa50015@gmail.com>
2019-05-02 18:26:32 +0200

    Try To Fix A problem

--- app/Product.php
+++ app/Product.php
@@ -3,12 +3,43 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Gloudemans\Shoppingcart\Contracts\Buyable;
 
-class Product extends Model
+class Product extends Model implements Buyable
 {
     public function presentPrice()
     {
         $price = $this->product_price;
         return "$" . $price;
+    }
+
+    /**
+     * Get the identifier of the Buyable item.
+     *
+     * @return int|string
+     */
+    public function getBuyableIdentifier($options = null)
+    {
+        return $this->product_id;
+    }
+
+    /**
+     * Get the description or title of the Buyable item.
+     *
+     * @return string
+     */
+    public function getBuyableDescription($options = null)
+    {
+        return $this->product_name;
+    }
+
+    /**
+     * Get the price of the Buyable item.
+     *
+     * @return float
+     */
+    public function getBuyablePrice($options = null)
+    {
+        return $this->product_price;
     }
 }
